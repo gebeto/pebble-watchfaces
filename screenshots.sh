2fa529930765465a6f094f48a1387110e3d4ec9f
gebeto <slavik.nychkalo@gmail.com>
2026-02-02 10:33:45 +0200

    add more screenshotys

--- screenshots.sh
+++ screenshots.sh
@@ -1,6 +1,25 @@
-cd a158
-pebble kill
-make run
-sleep 3
-mkdir -p assets
-pebble screenshot assets/screenshot.png --no-open
+#! /bin/bash
+
+function make_screenshot() {
+    dir=$1;
+    echo "Processing $dir"
+    cd $dir
+    pebble kill
+    make run
+    sleep 3
+    mkdir -p assets
+    pebble screenshot assets/screenshot.png --no-open
+    cd ..
+}
+
+if [ "$1" == 'all' ]; then
+    echo "Do All"
+    # for dir in */ ; do
+    #     make_screenshot dir
+    # done
+elif [ -d "$1" ]; then
+    make_screenshot $1
+else
+    echo "No command. Exiting..."
+fi
+
