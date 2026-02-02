dca21f279f4c67626b5dfe7a07fb7aed6a81a111
gebeto <slavik.nychkalo@gmail.com>
2026-02-02 10:21:19 +0200

    upd

--- screenshots.sh
+++ screenshots.sh
@@ -2,6 +2,5 @@ cd a158
 pebble kill
 make run
 sleep 3
-pebble screenshot assets/screenshot.png --no-open
 mkdir -p assets
-mv pebble_screenshot_* assets/screenshot.png
+pebble screenshot assets/screenshot.png --no-open
